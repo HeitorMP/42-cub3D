c7e2ae5938b952a759a26083b97ab202953926cf
Heitor Maciel Pinto <hmaciel-@c1r14s2.42porto.com>
2023-07-26 17:26:02 +0100

    start to fix

--- v2/srcs/move/straight.c
+++ v2/srcs/move/straight.c
@@ -6,7 +6,7 @@
 /*   By: hmaciel- <hmaciel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 14:36:35 by hmaciel-          #+#    #+#             */
-/*   Updated: 2023/07/26 13:50:14 by hmaciel-         ###   ########.fr       */
+/*   Updated: 2023/07/26 15:42:45 by hmaciel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,12 @@ void	move_forward(t_root *game)
 
 	current = game->map[(int)(game->player.y_pos + \
 		game->player.dir_x * MARGIN)][(int)game->player.x_pos];
-	if (current == '0' || current == 'O'){
-		printf("aqui\n");
-		game->player.y_pos += game->player.dir_x * game->move_speed;}
+	if (current == '0' || current == 'O')
+		game->player.y_pos += game->player.dir_x * game->move_speed;
 	current = game->map[(int)(game->player.y_pos)][(int) \
 		(game->player.x_pos + game->player.dir_y * MARGIN)];
 	if (current == '0' || current == 'O')
-	{
-		printf("aqui2\n");
 		game->player.x_pos += game->player.dir_y * game->move_speed;
-	}
 }
 
 void	move_backward(t_root *game)	
